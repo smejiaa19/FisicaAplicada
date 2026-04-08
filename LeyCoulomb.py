8033f9f7443723570d952e3b4c48c98aa4459968
smejiaa19 <silviomejiagarcia@gmail.com>
2026-04-08 11:20:49 -0600

    Actualizando cosas

--- LeyCoulomb.py
+++ LeyCoulomb.py
@@ -3,14 +3,27 @@ Permitir introducir n cargas junto posiciones (pares ordenados)
 Calcular vector fuerza neta < , > N y su magnitud
 '''
 
+import math
 
+class Carga:
+    def __init__(self, magnitud, x, y):
+        self.magnitud = magnitud
+        self.x = x
+        self.y = y
 
-def Input_info():
-    Carga_objetivo = []
-    magnitud = int(input('Ingesa la magnitud de la carga objetivo: \n'))
-    posicion = int(input('Ingrese la posicion de la carga objetivo: \n'))
-    Carga_objetivo.append(magnitud)
-    Carga_objetivo.append(posicion)
-    print(Carga_objetivo)    
+    def posicion(self):
+        return (self.x, self.y)
 
-Input_info()
+    def obtener_distancia (self, carga):
+        distancia_x = carga.x - self.x
+        distancia_y = carga.y - self.y
+        r = math.sqrt(distancia_x ** 2 + distancia_y ** 2)
+        return r
+    
+def input_cargas():
+    n = int(input('Ingrese el numero de cargas: \n'))
+    for i in range (n):
+        print(f'Informacion Carga {n+1}\n')
+        magnitud = float(input('Ingrese la magnitud de la carga: \n'))
+        x = float(input('Ingrese la coordenada x de la carga: \n'))
+        y = float(input('Ingrese la coordenada y de la carga: \n'))
